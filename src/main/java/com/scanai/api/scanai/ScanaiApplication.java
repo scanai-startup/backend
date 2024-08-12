5bd82d8a15cb6c66a6baefcc7173f88cd869bffc
DevAlencar <arthur.alencartrindade@gmail.com>
2024-08-12 14:43:46 -0300

    first commit

---
+++ src/main/java/com/scanai/api/scanai/ScanaiApplication.java
@@ -0,0 +1,13 @@
+package com.scanai.api.scanai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scanai/api/scanai/ScanaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanai.api.scanai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
