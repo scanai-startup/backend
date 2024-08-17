ff02edcba7d52eadf530ade7b55b998996146dce
DevAlencar <arthur.alencartrindade@gmail.com>
2024-08-17 11:29:45 -0300

    feature de alterar senha do usuario

--- src/main/java/com/scanai/api/controllers/FuncionarioController.java
+++ src/main/java/com/scanai/api/controllers/FuncionarioController.java
@@ -1,21 +1,35 @@
 package com.scanai.api.controllers;
 
+import com.scanai.api.domain.funcionario.dto.ResetPasswordDTO;
+import com.scanai.api.repositories.FuncionarioRepository;
 import com.scanai.api.services.FuncionarioService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
 
     @Autowired
-    FuncionarioService funcionarioService;
+    FuncionarioService service;
 
+    @Autowired
+    FuncionarioRepository repository;
 
+    @PutMapping("/updatePassword")
+    @Transactional
+    public ResponseEntity updatePassword(@RequestBody @Valid ResetPasswordDTO data){
+        service.resetPassword(data);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @Transactional
+    public ResponseEntity delete(@PathVariable Long id){
+        repository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
