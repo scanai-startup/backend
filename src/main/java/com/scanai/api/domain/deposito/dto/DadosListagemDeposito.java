c7c284272ea5993b7504056aa31423b2d57cd1e3
viniciuslevi <viniciuslevi152@gmail.com>
2024-11-06 15:34:14 -0300

    ajustando dto de detalhamento e listagem de deposito pra retornar o id

--- src/main/java/com/scanai/api/domain/deposito/dto/DadosListagemDeposito.java
+++ src/main/java/com/scanai/api/domain/deposito/dto/DadosListagemDeposito.java
@@ -2,10 +2,10 @@ package com.scanai.api.domain.deposito.dto;
 
 import com.scanai.api.domain.deposito.Deposito;
 
-public record DadosListagemDeposito(String tipo, String numero, boolean valid) {
+public record DadosListagemDeposito(Long id, String tipo, String numero, boolean valid) {
     public DadosListagemDeposito(Deposito deposito) {
         this(
-                deposito.getTipodeposito(), deposito.getNumerodeposito(), deposito.isValid()
+                deposito.getId(), deposito.getTipodeposito(), deposito.getNumerodeposito(), deposito.isValid()
         );
     }
 }
