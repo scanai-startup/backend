bdff8c99f26ed48b3aa0cd8bba0b400f24f52eee
viniciuslevi <viniciuslevi152@gmail.com>
2025-02-18 19:10:18 -0300

    ajustes para fazer o deploy

--- src/main/resources/db/migration/V39__alter-table-material-and-entrada.sql
+++ src/main/resources/db/migration/V39__alter-table-material-and-entrada.sql
@@ -1,5 +1,5 @@
-ALTER TABLE scanAiDev.tb_entradamaterial DROP FOREIGN KEY fk_entrada_material_1;
-ALTER TABLE scanAiDev.tb_entradamaterial DROP COLUMN `fkmaterial`;
+ALTER TABLE tb_entradamaterial DROP FOREIGN KEY fk_entrada_material_1;
+ALTER TABLE tb_entradamaterial DROP COLUMN `fkmaterial`;
 
-ALTER TABLE scanAiDev.tb_lotematerial ADD COLUMN `fkmaterial` INT NOT NULL;
-ALTER TABLE scanAiDev.tb_lotematerial ADD CONSTRAINT `fk_lotematerial_material` FOREIGN KEY (`fkmaterial`) REFERENCES tb_material(`id`);
+ALTER TABLE tb_lotematerial ADD COLUMN `fkmaterial` INT NOT NULL;
+ALTER TABLE tb_lotematerial ADD CONSTRAINT `fk_lotematerial_material` FOREIGN KEY (`fkmaterial`) REFERENCES tb_material(`id`);
